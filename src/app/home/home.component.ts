79007f47415dc644e7fb960ac79d8c020a4540ce
Terry Robinson <tdrobinson94@comcast.net>
2020-08-24 16:02:52 -0400

    updated location of buttons on Login and Signup pages

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import $ from 'jquery';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    $('html, body').animate({ scrollTop: 0 }, 500);
+  }
+
+}
