331306141484d5277be50ea4d7fd76555a4fdd4a
Terry Robinson <tdrobinson94@comcast.net>
2020-09-19 09:37:38 -0400

    added video tutorial to homepage with scroll features for mobile

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import $ from 'jquery';
+import 'hammerjs';
 
 @Component({
   selector: 'app-home',
@@ -12,6 +13,31 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     $('html, body').animate({ scrollTop: 0 }, 500);
+  }
+
+  scrolltoVideo() {
+    $('.home-buttons').addClass('hide');
+    $('html, body').animate({ scrollTop: $(window).height() }, 500);
+    setTimeout(() => {
+      $('.video').get(0).play();
+    }, 550);
+  }
+
+  onSwipeDown(e) {
+    if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i)
+      || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i)) {
+        console.log('Swipe Down');
+        $('.video').get(0).pause();
+        $('html, body').animate({ scrollTop: 0 }, 500);
+        $('.home-buttons').removeClass('hide');
+      }
+  }
+
+  onSwipeUp(e) {
+    if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i)
+      || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i)) {
+        console.log('Swipe Up');
+      }
   }
 
 }
