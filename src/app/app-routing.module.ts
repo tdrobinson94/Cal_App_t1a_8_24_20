972914e191131657aa953e5dbe0e79dd056f5141
Terry Robinson <tdrobinson94@comcast.net>
2020-09-20 13:57:02 -0400

    start_url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,7 @@ const routes: Routes = [
   { path: 'settings', component: SettingsComponent, canActivate: [AuthGuard] },
   { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
   { path: 'test-cal', component: TestCalendarComponent, canActivate: [AuthGuard] },
-  { path: '', component: HomeComponent, canActivate: [GuestGuard] }
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
